5ee2d73f3969a67bf7f20233c5fe80e9383761bc
Matthew Foale <foalematthew@gmail.com>
2015-01-15 20:42:35 +0000

    Change Matthew to red

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,7 @@
 class HomeController < ApplicationController
  
- respond_to :json
+ 
 
   def index
-
-render :text => 
-
-    '{
-          "info":   [
-           {
-          "Name":"MATTHEW"
-            },
-            {
-          "Age":"14 this Year!"
-            },
-            {
-          "Height":"165cm"
-            }
-
-       			     ]
-
-
-     }'
-
-
-
   end
 end
